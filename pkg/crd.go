a460a2015bffad617459f627ca4273f4b5605ce6
amrom <linjinbao666@gmail.com>
2022-06-20 15:49:21 +0800

    update

--- pkg/crd.go
+++ pkg/crd.go
@@ -1,8 +1,11 @@
 package pkg
 
 import (
+	"context"
+	"fmt"
 	"log"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -23,9 +26,16 @@ func Watch(kubeconfig string) {
 	if err != nil {
 		panic(err)
 	}
-	_, err = kubernetes.NewForConfig(config)
+	podClient, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
 
+	podList, err := podClient.CoreV1().Pods("kube-system").List(context.TODO(), v1.ListOptions{})
+	if err != nil {
+		log.Panicln(err)
+	}
+	for _, v := range podList.Items {
+		fmt.Println(v.Name)
+	}
 }
