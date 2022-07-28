2a0a23e12294919dd2e1ff7df47e6a0475211e2a
amrom <linjinbao666@gmail.com>
2022-07-28 17:52:56 +0800

    update log output

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,8 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/golang/glog"
@@ -51,19 +53,30 @@ func initConfig() {
 		viper.SetConfigFile(cfgFile)
 	} else {
 		pwd, err := os.Getwd()
-		glog.Infoln("Using config from path:", pwd)
 		cobra.CheckErr(err)
-
 		viper.AddConfigPath(pwd)
 		viper.SetConfigType("yaml")
 		viper.SetConfigName(".dt-runner")
 	}
 	viper.SetDefault("app.name", "dt-runner")
-	viper.SetDefault("app.log", "/var/log/dt-runner.log")
+	viper.SetDefault("app.log", "/var/log/dt-runner")
 	viper.SetDefault("server.host", "127.0.0.1")
 	viper.SetDefault("server.port", 9001)
 	viper.SetDefault("server.runtime", "kubernetes")
 	if err := viper.ReadInConfig(); err == nil {
+		logPath := viper.GetString("app.log")
+		_, err := os.Stat(logPath)
+		if err != nil {
+			err := os.MkdirAll(logPath, 0766)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+		defer glog.Flush()
+		flag.Set("log_dir", logPath)
+		flag.Set("alsologtostderr", "true")
+		flag.Parse()
 		glog.Infoln("Using config file:", viper.ConfigFileUsed())
 	}
 }
